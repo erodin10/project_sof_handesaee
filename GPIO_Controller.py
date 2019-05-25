e1afb82706ee1b9d84aab6ba71f75e7240bcfc3f
Effi Rodin <erodin10@gmail.com>
2019-05-25 22:53:39 +0300

    Update GPIO_Controller.py

--- GPIO_Controller.py
+++ GPIO_Controller.py
@@ -6,8 +6,6 @@ import time
 """
 Decode the voice recognizer
 
-VR Module should be working in pulse mode(Negative pulse).
-
 Group 1 i.e. [0,1]:
     *   Call
     *   Unlock
@@ -23,28 +21,25 @@ Group 2 i.e. [1,0]:
     *   Unlock
 
 Group 3 i.e. [1,1]:
-    *NOT USED IN THIS PROJECT
+    *
 
 
 
 """
 
-global group #Holds CURRENT group number
-global sentence #Holds Sentence recognized from module
+global group
+global sentence
 
-
-"""
-Each function should be called when interrupted by VR Module's GPIO corresponding to the name of the function.
-Eg. if GPIO 1 goes low, then function one() gets called etc. etc.
-"""
 def one(channel):
+    global group
     if group == [0,1]:
         sentence += "Call Room"
     elif group == [1,0]:
         sentence += "Answer Call"
     elif group == [1,1]:
-        pass
+        sentence += ""
 def two(channel):
+    global group
     if group == [0,1]:
         sentence += "Unlock"
     elif group == [1,0]:
@@ -52,6 +47,7 @@ def two(channel):
     elif group == [1,1]:
         pass
 def three(channel):
+    global group
     if group == [0,1]:
         sentence+="Bolt"
     elif group == [1,0]:
@@ -73,16 +69,6 @@ def five(channel):
     elif group == [1,1]:
         pass
 
-
-"""
-Import different groups for Voice recognition.
-
-parameter:
-  Group 1: [0,1]
-    "   2: [1,0]
-    "   3: [1,1]
-  
-"""
 def call_group(group):
     GPIO.output(36, group[0]) #higher order
     GPIO.output(37, group[1]) #lower order
@@ -94,6 +80,7 @@ def call_group(group):
 
 
 def main():
+    global group
     GPIO.setmode(GPIO.BOARD)
 
     GPIO.setup(36, GPIO.OUT)
@@ -106,9 +93,6 @@ def main():
 
     funcs = [one, two, three, four, five] #list of functions for each interrupt
 
-    for count,pin in enumerate([29, 31, 32, 33, 35]): #Asign the correct interrupt function to RBPi's corresponding GPIO's(in the list)
-        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) #in pull up because RS232 is '1' when idle
-        GPIO.add_event_detect(pin, GPIO.FALLING, callback=funcs[count]) #assign function to pin event
 
 
     while 1:
